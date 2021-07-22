f74fa8d22c39d937fc8eef78ad68bf0b568fb4ec
Achimeir Shenwald <achimeir.shenwald@gmail.com>
2021-07-23 01:00:09 +0300

    React blog with multiple articles using react components

---
+++ fs101/src/pages/ArticlePage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import articles from './article-content';
+import articleContent from './article-content'
+import ArticlesList from '../components/ArticlesList';
+import NotFoundPage from './NotFoundPage';
+
+const ArticlePage = ({ match }) => {
+    const name = match.params.name;
+    const article = articleContent.find(article => article.name == name)
+    if (!article) return <NotFoundPage/>
+    const otherArticles = articleContent.filter(article => article.name != name)
+    return (
+        <React.Fragment>
+            <h1>{article.title}</h1>
+            {article.content.map((paragraph, key) => (
+                <p key={key}>{paragraph}</p>
+            ))}
+            <h3>Other articles:</h3>
+            <ArticlesList articles={otherArticles} />
+        </React.Fragment>
+    );
+}
+
+export default ArticlePage;
---
+++ fs101/src/pages/NotFoundPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+const NotFoundPage = () => (
+    <React.Fragment>
+        <h1 className='not-found-header'> OOPS, Page Not Found...</h1>
+        <img src={'https://wpklik.com/wp-content/uploads/2019/03/A-404-Page-Best-Practices-and-Design-Inspiration.jpg'}/>
+    </React.Fragment>
+    
+);
+
+export default NotFoundPage;
