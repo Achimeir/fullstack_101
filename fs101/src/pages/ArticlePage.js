b6e21d5f25db10130141912de6b68012707915dc
Achimeir Shenwald <achimeir.shenwald@gmail.com>
2021-07-25 00:06:47 +0300

    add upvote and comment components plus connect the server to the client side

--- fs101/src/pages/ArticlePage.js
+++ fs101/src/pages/ArticlePage.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import articles from './article-content';
 import articleContent from './article-content'
 import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from './NotFoundPage';
+import CommentsList from '../components/commentsList';
+import UpvoteSection from '../components/UpvoteSection';
+import AddCommentsForm from '../components/AddCommentForm';
 
 const ArticlePage = ({ match }) => {
     const name = match.params.name;
-    const article = articleContent.find(article => article.name == name)
-    if (!article) return <NotFoundPage/>
+    const article = articleContent.find(article => article.name == name);
+
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+    useEffect(()=>{
+        const fetchData = async ()=>{
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json();
+            setArticleInfo(body);
+        }
+        fetchData();
+    },[name]);
+
+    if (!article) return <NotFoundPage />
     const otherArticles = articleContent.filter(article => article.name != name)
     return (
         <React.Fragment>
             <h1>{article.title}</h1>
+            <UpvoteSection articleName={name} upvotes={articleInfo.upvotes} setArticleInfo={setArticleInfo}/>
             {article.content.map((paragraph, key) => (
                 <p key={key}>{paragraph}</p>
             ))}
+            <CommentsList comments={articleInfo.comments}/>
+            <AddCommentsForm articleName={name} setArticleInfo={setArticleInfo} />
             <h3>Other articles:</h3>
             <ArticlesList articles={otherArticles} />
         </React.Fragment>
